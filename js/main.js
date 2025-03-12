fc07fcff6836f764880b01a4f2e02349d1c486bc
dbaustin4 <dbuanahagi4@gmail.com>
2025-03-12 12:42:22 +0100

    all products page and main.js changes

---
+++ js/main.js
@@ -0,0 +1,92 @@
+const products = [
+    { 
+      id: '1', 
+      name: 'Barbour Barlow Flatcap', 
+      price: 44.90, 
+      image: 'img/men/menhat.png', 
+    },
+    { 
+      id: '2', 
+      name: '7V Battery Heated Jacket', 
+      price: 76.95, 
+      image: 'img/men/menjacket.jpg', 
+    },
+    { 
+      id: '3', 
+      name: 'Satu Adventure Pants', 
+      price: 89.99, 
+      image: 'img/men/menpants.png', 
+    },
+    { 
+      id: '4', 
+      name: 'General Construction Work Gloves', 
+      price: 19.99, 
+      image: 'img/men/mengloves.jpg', 
+    },
+    { 
+      id: '5', 
+      name: 'ELTEN Sicherheitsstiefel STEFANO XXSG', 
+      price: 148.90, 
+      image: 'img/men/menshoes.jpg', 
+    },
+    { 
+      id: '6', 
+      name: 'Uposao Fur Hat', 
+      price: 20.90, 
+      image: 'img/women/womenhat.jpg', 
+    },
+    { 
+      id: '7', 
+      name: 'iheat Heated Jacket', 
+      price: 100.95, 
+      image: 'img/women/womenjacket.jpg', 
+    },
+    { 
+      id: '8', 
+      name: 'CRZ YOGA Cross Waist Leggings', 
+      price: 35.99, 
+      image: 'img/women/womenleggs.jpg', 
+    },
+    { 
+      id: '9', 
+      name: 'Battery Powered Heated Socks', 
+      price: 50.65, 
+      image: 'img/women/womensocks.jpg', 
+    },
+    { 
+      id: '10', 
+      name: 'Even&Odd Platform Boots', 
+      price: 48.90, 
+      image: 'img/women/womenboots.png', 
+    },
+    { 
+      id: '11', 
+      name: 'Andersson Powerbank 10000mAh', 
+      price: 24.99, 
+      image: 'img/electr/powerbank.jpg', 
+    },
+    { 
+      id: '12', 
+      name: 'Lenovo Legion VR Glasses', 
+      price: 349.99, 
+      image: 'img/electr/glasses.png', 
+    },
+    { 
+      id: '13', 
+      name: 'RingConn Smart Ring', 
+      price: 284.90, 
+      image: 'img/electr/ring.png', 
+    },
+    { 
+      id: '14', 
+      name: 'Anker USB-C to USB-C Cable', 
+      price: 25.50, 
+      image: 'img/electr/cable.jpg', 
+    },
+    { 
+      id: '15', 
+      name: 'EarFun Air Pro 3 Earbuds', 
+      price: 49.99, 
+      image: 'img/electr/earbuds.jpg', 
+    }
+  ];
