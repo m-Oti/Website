2c5fa2fca240468286f4f2fdc1ac26feb90a4d4d
mahtis26 <zama24zw@student.ju.se>
2025-03-13 18:36:17 +0100

    Updated responsiveness of the navbar

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,25 @@
-var prevScrollpos = window.scrollY;
+document.addEventListener("DOMContentLoaded", function () {
+  var prevScrollpos = window.scrollY;
 
-window.addEventListener("scroll", function () {
-  var currentScrollPos = window.scrollY;
-  if (prevScrollpos < currentScrollPos) {
-    document.querySelector("header").classList.add("hidden");
-  } else if (prevScrollpos > currentScrollPos) {
-    document.querySelector("header").classList.remove("hidden");
-  }
-  prevScrollpos = currentScrollPos;
+  window.addEventListener("scroll", function () {
+    var currentScrollPos = window.scrollY;
+    if (prevScrollpos < currentScrollPos) {
+      document.querySelector("header").classList.add("hidden");
+    } else if (prevScrollpos > currentScrollPos) {
+      document.querySelector("header").classList.remove("hidden");
+    }
+    prevScrollpos = currentScrollPos;
+  });
+
+  document
+    .querySelector("#hamburger-trigger")
+    .addEventListener("click", function () {
+      document.querySelector("header").classList.add("menu-expanded");
+    });
+
+  document
+    .querySelector("#close-trigger")
+    .addEventListener("click", function () {
+      document.querySelector("header").classList.remove("menu-expanded");
+    });
 });
