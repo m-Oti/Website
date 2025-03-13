6d774f2c40e65d7ffde1eb332804e657808f8246
mahtis26 <zama24zw@student.ju.se>
2025-03-14 00:05:37 +0100

    Updated js to now click on card, not the button. Deleted View Details button. Deleted Placeholders in HTML file

--- js/main.js
+++ js/main.js
@@ -11,28 +11,18 @@ document.addEventListener("DOMContentLoaded", () => {
       const productCard = document.createElement("div");
       productCard.classList.add("product-card");
 
+      // Add event listeners to "View Details" buttons
+      productCard.addEventListener("click", (e) => {
+        window.location.href = `detail.html?product=${product.id}`;
+      });
+
       productCard.innerHTML = `
                 <img src="${product.image}" alt="${product.name}">
                 <div class="product-price">€ ${product.price.toFixed(2)}</div>
                 <div class="product-name">${product.name}</div>
-                <div class="product-tag">In Stock</div>
-                <button class="view-details" data-id="${
-                  product.id
-                }">View Details</button>
-            `;
-
+        `;
       productGrid.appendChild(productCard);
     });
-
-    // Add event listeners to "View Details" buttons
-    document.querySelectorAll(".view-details").forEach((button) => {
-      button.addEventListener("click", (e) => {
-        const productId = e.target.getAttribute("data-id");
-        localStorage.setItem("SELECTED_PRODUCT_KEY", productId);
-        window.location.href = "detailearph.html";
-      });
-    });
   }
-
   displayProducts();
 });
